ed2bbe0e3648199f9e5f7bd2a5d4cf27659e3997
apolo176 <neder176@gmail.com>
2026-05-08 10:18:05 +0200

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/FootballDSS/',
+  base: '/FootballPWA/',
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#0f172a',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/FootballDSS/',
-        scope: '/FootballDSS/',
+        start_url: '/FootballPWA/',
+        scope: '/FootballPWA/',
         icons: [
           { src: 'images.png', sizes: '192x192', type: 'image/png' },
           { src: 'images.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
